d1a8a47680b399432118b89ee0e6c266faccd0bf
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2022-06-03 15:24:48 -0400

    Add routes to menu

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,26 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
+@app.route('/aboutMe')
+def aboutMe():
+    return render_template('about_me.html', title="MLH Fellow")
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html', title="MLH Fellow")
+
+@app.route('/travel')
+def travel():
+    return render_template('travel.html', title="MLH Fellow")
 
 @app.route('/hobbies')
-def index():
-    return render_template('hobbies.html', title="MLH Fellow", url=os.getenv("URL"))
+def hobbies():
+    return render_template('hobbies.html', title="MLH Fellow")
+
+@app.route('/education')
+def education():
+    return render_template('education.html', title="MLH Fellow")
