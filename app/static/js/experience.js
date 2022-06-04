80f988caa898cfe05589ccc93c0e4ec6160501d4
hussaino03 <m.hussainomer03@gmail.com>
2022-06-04 16:04:02 -0400

    Added Hobbies Section

--- app/static/js/experience.js
+++ app/static/js/experience.js
@@ -6,3 +6,24 @@ $('.about-me-img img, .authorWindowWrapper').hover(function() {
 }, function() {
   //$('.authorWindowWrapper').stop().css('display', 'none').find('p').removeClass('trans');
 });
+
+$(window).scroll(function(){				 
+	$('.container p').each(function(){
+    	var scrollTop     = $(window).scrollTop(),
+        	elementOffset = $(this).offset().top,
+       		distance      = (elementOffset - scrollTop),
+			    windowHeight  = $(window).height(),
+			    breakPoint    = windowHeight*0.9;
+
+			if(distance > breakPoint) {
+				$(this).addClass("more-padding");	
+			}  if(distance < breakPoint) {
+				$(this).removeClass("more-padding");	
+			}
+	});
+});
+
+
+
+
+
