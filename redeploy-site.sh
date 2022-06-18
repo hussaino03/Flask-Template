080f319225d164cb5db0a22141f6ddae7093236f
Hussain Omer <67332652+hussaino03@users.noreply.github.com>
2022-06-18 15:12:59 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 pkill -f tmux
-cd team-portfolio
+cd team-portfolio
 git fetch && git reset origin/main --hard
 pip3 install -r requirements.txt
-tmux new -d -s pfolio-session
-cd team-portfolio
+tmux new -d 'flask run --host=0.0.0.0'
+tmux attach
+cd /root/team-portfolio
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 export FLASK_ENV=development
-flask run 
+flask run --host=0.0.0.0
