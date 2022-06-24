03d6fdf81ad88dc112d6af5105a897731ce2733d
Hussain Omer <67332652+hussaino03@users.noreply.github.com>
2022-06-24 08:51:49 -0400

    Update curl-test.sh

--- curl-test.sh
+++ curl-test.sh
@@ -1,1 +1,5 @@
-ex
+#!/bin/bash
+
+curl --request GET http://localhost:5000/api/timeline_post
+
+curl --request POST http://localhost:5000/api/timeline_post -d 'name=Hussain&email=Hussain@gmail.com&content=Just Added Dtatbase to my portfolio site'
