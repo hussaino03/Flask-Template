018fd2e316f5f17c0a9e835df566fa616d763395
Hussain Omer <67332652+hussaino03@users.noreply.github.com>
2022-07-10 19:01:29 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,5 @@
-#!/usr/bin/env bash
-
 cd team-portfolio
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip3 install -r requirements.txt
-systemctl restart myportfolio2
+
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
