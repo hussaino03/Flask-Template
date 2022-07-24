bebc173622877fd43e1872edeae53c0ede7c6cd5
Hussain Omer <67332652+hussaino03@users.noreply.github.com>
2022-07-23 21:43:39 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-cd /root/team-portfolio
+cd ~/team-portfolio
 git fetch && git reset origin/main --hard
-
 docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml up -d --build
+docker compose -f docker-compose.prod.yml up -d --build 
